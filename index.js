60f4310242ed4278c52e0922b5b1472460c501a2
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-20 15:16:32 +0800

    Add body parser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ strict: false, type() { return true; } }));
 
 app.post('/start', function(req, res) {
   if (req.query.secret != process.env.secret) {
